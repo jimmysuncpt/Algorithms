d26f65cb9dd49f6ba0c30a505a03f451a0b17af9
jimmysuncpt <jimmysuncpt@gmail.com>
2016-10-19 16:38:00 +0800

    Exercise 1.1.8

---
+++ src/com/jimmysun/algorithms/chapter1_1/Ex08.java
@@ -0,0 +1,9 @@
+package com.jimmysun.algorithms.chapter1_1;
+
+public class Ex08 {
+	public static void main(String[] args) {
+		System.out.println('b');
+		System.out.println('b' + 'c');
+		System.out.println((char) ('a' + 4));
+	}
+}
