f9ed58b0f2249d574df7e3162b7ee2b57a3c3a84
jimmysuncpt <jimmysuncpt@gmail.com>
2016-10-19 16:35:42 +0800

    Exercise 1.1.2

---
+++ src/com/jimmysun/algorithms/chapter1_1/Ex02.java
@@ -0,0 +1,10 @@
+package com.jimmysun.algorithms.chapter1_1;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println((1 + 2.236) / 2);
+		System.out.println(1 + 2 + 3 + 4.0);
+		System.out.println(4.1 >= 4);
+		System.out.println(1 + 2 + "3");
+	}
+}
