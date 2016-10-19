8b76261381937bbc74b07968779e050cebf39e69
jimmysuncpt <jimmysuncpt@gmail.com>
2016-10-19 16:35:01 +0800

    Exercise 1.1.1

---
+++ src/com/jimmysun/algorithms/chapter1_1/Ex01.java
@@ -0,0 +1,9 @@
+package com.jimmysun.algorithms.chapter1_1;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println((0 + 15) / 2);
+		System.out.println(2.0e-6 * 100000000.1);
+		System.out.println(true && false || true && true);
+	}
+}
