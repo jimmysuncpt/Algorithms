0cd533f59caa8b5bb2cc8d648bb56c0b81a727d9
jimmysuncpt <jimmysuncpt@gmail.com>
2016-10-19 16:36:20 +0800

    Exercise 1.1.5

---
+++ src/com/jimmysun/algorithms/chapter1_1/Ex05.java
@@ -0,0 +1,11 @@
+package com.jimmysun.algorithms.chapter1_1;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Ex05 {
+	public static void main(String[] args) {
+		double x = StdIn.readDouble();
+		double y = StdIn.readDouble();
+		System.out.println(x > 0 && x < 1 && y > 0 && y < 1);
+	}
+}
