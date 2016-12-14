41cfce61cfb51d42e9a84815c63cb8a5fe87a1b8
Jimmy Sun <jimmysuncpt@gmail.com>
2016-12-14 15:36:45 +0800

    Fixed Capacity Stack

--- src/com/jimmysun/algorithms/chapter1_3/FixedCapacityStack.java
+++ src/com/jimmysun/algorithms/chapter1_3/FixedCapacityStack.java
@@ -20,11 +20,27 @@ public class FixedCapacityStack<Item> {
 	}
 
 	public void push(Item item) {
+		if (N == a.length) {
+			resize(2 * a.length);
+		}
 		a[N++] = item;
 	}
 
 	public Item pop() {
-		return a[--N];
+		Item item = a[--N];
+		a[N] = null;
+		if (N > 0 && N == a.length / 4) {
+			resize(a.length / 2);
+		}
+		return item;
+	}
+
+	private void resize(int max) {
+		Item[] temp = (Item[]) new Object[max];
+		for (int i = 0; i < N; i++) {
+			temp[i] = a[i];
+		}
+		a = temp;
 	}
 
 	public static void main(String[] args) {
