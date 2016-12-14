8292960956b3caaab9c30b5600c5b53932f1b6f8
Jimmy Sun <jimmysuncpt@gmail.com>
2016-12-14 15:24:01 +0800

    Fixed Capacity Stack

---
+++ src/com/jimmysun/algorithms/chapter1_3/FixedCapacityStack.java
@@ -0,0 +1,43 @@
+package com.jimmysun.algorithms.chapter1_3;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FixedCapacityStack<Item> {
+	private Item[] a; // stack entries
+	private int N; // size
+
+	public FixedCapacityStack(int cap) {
+		a = (Item[]) new Object[cap];
+	}
+
+	public boolean isEmpty() {
+		return N == 0;
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public void push(Item item) {
+		a[N++] = item;
+	}
+
+	public Item pop() {
+		return a[--N];
+	}
+
+	public static void main(String[] args) {
+		FixedCapacityStack<String> s;
+		s = new FixedCapacityStack<String>(100);
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-")) {
+				s.push(item);
+			} else if (!s.isEmpty()) {
+				StdOut.print(s.pop() + " ");
+			}
+		}
+		StdOut.println("(" + s.size() + " left on stack)");
+	}
+}
