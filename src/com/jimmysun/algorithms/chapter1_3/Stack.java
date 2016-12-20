b660e5cf1a60102cd09ff181b26a9fd28e46a6a0
Jimmy Sun <jimmysuncpt@gmail.com>
2016-12-20 19:14:42 +0800

    下压堆栈

---
+++ src/com/jimmysun/algorithms/chapter1_3/Stack.java
@@ -0,0 +1,58 @@
+package com.jimmysun.algorithms.chapter1_3;
+
+import java.util.Iterator;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Stack<Item> implements Iterable<Item> {
+	private Node first;
+	private int N;
+
+	private class Node {
+		Item item;
+		Node next;
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public void push(Item item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
+	}
+
+	public Item pop() {
+		Item item = first.item;
+		first = first.next;
+		N--;
+		return item;
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Stack<String> s = new Stack<String>();
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (!item.equals("-")) {
+				s.push(item);
+			} else if (!s.isEmpty()) {
+				StdOut.print(s.pop() + " ");
+			}
+		}
+		StdOut.println("(" + s.size() + " left on stack)");
+	}
+}
