c56be25b1e2078a31d2d1a9a44027101da8ddad3
Jimmy Sun <jimmysuncpt@gmail.com>
2016-12-20 19:57:36 +0800

    Bag

---
+++ src/com/jimmysun/algorithms/chapter1_3/Bag.java
@@ -0,0 +1,54 @@
+package com.jimmysun.algorithms.chapter1_3;
+
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item> {
+	private Node first;
+	private int N;
+
+	private class Node {
+		Item item;
+		Node next;
+	}
+
+	public boolean isEmpty() {
+		return first == null;
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public void add(Item item) {
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		return new ListIterator();
+	}
+
+	private class ListIterator implements Iterator<Item> {
+		private Node current = first;
+
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public Item next() {
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+
+		@Override
+		public void remove() {
+		}
+	}
+}
