d07a556801be31a29e93cba9bf13bb8048745478
Jimmy Sun <jimmysuncpt@gmail.com>
2016-12-31 10:37:22 +0800

    Shell Sort

---
+++ src/com/jimmysun/algorithms/chapter2_1/Shell.java
@@ -0,0 +1,55 @@
+package com.jimmysun.algorithms.chapter2_1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Shell {
+	public static void sort(Comparable[] a) {
+		int N = a.length;
+		int h = 1;
+		while (h < N / 3) {
+			h = 3 * h + 1;
+		}
+		while (h >= 1) {
+			for (int i = h; i < N; i++) {
+				for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+					exch(a, j, j - h);
+				}
+			}
+			h = h / 3;
+		}
+	}
+
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+	private static void show(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			StdOut.print(a[i] + " ");
+		}
+		StdOut.println();
+	}
+
+	public static boolean isSorted(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			if (less(a[i], a[i - 1])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String[] a = new In().readAllStrings();
+		sort(a);
+		assert isSorted(a);
+		show(a);
+	}
+}
