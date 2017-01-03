9f490d257af52e10384c52697b8bf01c11afc829
Jimmy Sun <jimmysuncpt@gmail.com>
2017-01-03 20:48:07 +0800

    Merge Bottom Up Sort

---
+++ src/com/jimmysun/algorithms/chapter2_2/MergeBU.java
@@ -0,0 +1,69 @@
+package com.jimmysun.algorithms.chapter2_2;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class MergeBU {
+	private static Comparable[] aux;
+
+	public static void merge(Comparable[] a, int lo, int mid, int hi) {
+		int i = lo, j = mid + 1;
+		for (int k = lo; k <= hi; k++) {
+			aux[k] = a[k];
+		}
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid) {
+				a[k] = aux[j++];
+			} else if (j > hi) {
+				a[k] = aux[i++];
+			} else if (less(aux[j], aux[i])) {
+				a[k] = aux[j++];
+			} else {
+				a[k] = aux[i++];
+			}
+		}
+	}
+
+	public static void sort(Comparable[] a) {
+		int N = a.length;
+		aux = new Comparable[N];
+		for (int sz = 1; sz < N; sz = sz + sz) {
+			for (int lo = 0; lo < N - sz; lo += sz + sz) {
+				merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+			}
+		}
+	}
+
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+	private static void show(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			StdOut.print(a[i] + " ");
+		}
+		StdOut.println();
+	}
+
+	public static boolean isSorted(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			if (less(a[i], a[i - 1])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String[] a = new In().readAllStrings();
+		sort(a);
+		assert isSorted(a);
+		show(a);
+	}
+}
