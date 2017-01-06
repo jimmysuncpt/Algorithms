e353b6a5a83afb3b92e1f26a2c068d399e1e20ac
Jimmy Sun <jimmysuncpt@gmail.com>
2017-01-06 20:30:19 +0800

    Quick Sort

---
+++ src/com/jimmysun/algorithms/chapter2_3/Quick.java
@@ -0,0 +1,77 @@
+package com.jimmysun.algorithms.chapter2_3;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Quick {
+	public static void sort(Comparable[] a) {
+		StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
+	}
+
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if (hi <= lo) {
+			return;
+		}
+		int j = partition(a, lo, hi);
+		sort(a, lo, j - 1);
+		sort(a, j + 1, hi);
+	}
+
+	private static int partition(Comparable[] a, int lo, int hi) {
+		int i = lo, j = hi + 1;
+		Comparable v = a[lo];
+		while (true) {
+			while (less(a[++i], v)) {
+				if (i == hi) {
+					break;
+				}
+			}
+			while (less(v, a[--j])) {
+				if (j == lo) {
+					break;
+				}
+			}
+			if (i >= j) {
+				break;
+			}
+			exch(a, i, j);
+		}
+		exch(a, lo, j);
+		return j;
+	}
+
+	private static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+
+	private static void show(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			StdOut.print(a[i] + " ");
+		}
+		StdOut.println();
+	}
+
+	public static boolean isSorted(Comparable[] a) {
+		for (int i = 0; i < a.length; i++) {
+			if (less(a[i], a[i - 1])) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String[] a = new In().readAllStrings();
+		sort(a);
+		assert isSorted(a);
+		show(a);
+	}
+}
