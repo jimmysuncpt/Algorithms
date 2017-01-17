f8b1dd7508b0166c2cc0c14ca8b1796eb78c296d
Sun Qiang <jimmysuncpt@qq.com>
2017-01-17 15:52:42 +0800

    Exercise 1.1.9

--- src/com/jimmysun/algorithms/chapter1_1/Ex09.java
+++ src/com/jimmysun/algorithms/chapter1_1/Ex09.java
@@ -2,10 +2,10 @@ package com.jimmysun.algorithms.chapter1_1;
 
 public class Ex09 {
 	public static String toBinaryString(int N) {
-		String s = "";
-		if(N==0){
+		if (N == 0) {
 			return "0";
 		}
+		String s = "";
 		while (N > 0) {
 			s = N % 2 + s;
 			N /= 2;
