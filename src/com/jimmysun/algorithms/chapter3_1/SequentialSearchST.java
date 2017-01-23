7e9e12fd1c921e14ba4a30f2aced67b9991865d8
Sun Qiang <jimmysuncpt@qq.com>
2017-01-23 15:10:22 +0800

    Exercise 3.1.5

--- src/com/jimmysun/algorithms/chapter3_1/SequentialSearchST.java
+++ src/com/jimmysun/algorithms/chapter3_1/SequentialSearchST.java
@@ -1,7 +1,10 @@
 package com.jimmysun.algorithms.chapter3_1;
 
+import com.jimmysun.algorithms.chapter1_3.Queue;
+
 public class SequentialSearchST<Key, Value> {
 	private Node first;
+	private int N = 0;
 
 	private class Node {
 		Key key;
@@ -32,5 +35,35 @@ public class SequentialSearchST<Key, Value> {
 			}
 		}
 		first = new Node(key, val, first);
+		N++;
+	}
+
+	// Exercise 3.1.5
+	public int size() {
+		return N;
+	}
+
+	public void delete(Key key) {
+		first = delete(first, key);
+	}
+
+	private Node delete(Node x, Key key) {
+		if (x == null) {
+			return null;
+		}
+		if (x.key.equals(key)) {
+			N--;
+			return x.next;
+		}
+		x.next = delete(x.next, key);
+		return x;
+	}
+
+	public Iterable<Key> keys() {
+		Queue<Key> queue = new Queue<>();
+		for (Node x = first; x != null; x = x.next) {
+			queue.enqueue(x.key);
+		}
+		return queue;
 	}
 }
