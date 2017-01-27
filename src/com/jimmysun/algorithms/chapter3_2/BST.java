e5f07269cf60a2fdd7ef5f6f5e24e17756f2c21b
Jimmy Sun <jimmysuncpt@gmail.com>
2017-01-27 09:59:14 +0800

    Exercise 3.2.32

--- src/com/jimmysun/algorithms/chapter3_2/BST.java
+++ src/com/jimmysun/algorithms/chapter3_2/BST.java
@@ -273,4 +273,22 @@ public class BST<Key extends Comparable<Key>, Value> {
 		}
 		return 1 + Math.max(height(x.left), height(x.right));
 	}
+
+	// Exercise 3.2.32
+	private boolean isBST() {
+		return isBST(root, null, null);
+	}
+
+	private boolean isBST(Node x, Key min, Key max) {
+		if (x == null) {
+			return true;
+		}
+		if (min != null && x.key.compareTo(min) <= 0) {
+			return false;
+		}
+		if (max != null && x.key.compareTo(max) >= 0) {
+			return false;
+		}
+		return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+	}
 }
