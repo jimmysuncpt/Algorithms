8d97288b4ec5aceb04b883a98e35f9c31d429add
Jimmy Sun <jimmysuncpt@gmail.com>
2017-02-17 11:04:22 +0800

    Directed Cycle

---
+++ src/com/jimmysun/algorithms/chapter4_2/DirectedCycle.java
@@ -0,0 +1,51 @@
+package com.jimmysun.algorithms.chapter4_2;
+
+import com.jimmysun.algorithms.chapter1_3.Stack;
+
+public class DirectedCycle {
+	private boolean[] marked;
+	private int[] edgeTo;
+	private Stack<Integer> cycle;
+	private boolean[] onStack;
+
+	public DirectedCycle(Digraph G) {
+		onStack = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		marked = new boolean[G.V()];
+		for (int v = 0; v < G.V(); v++) {
+			if (!marked[v]) {
+				dfs(G, v);
+			}
+		}
+	}
+
+	private void dfs(Digraph G, int v) {
+		onStack[v] = true;
+		marked[v] = true;
+		for (int w : G.adj(v)) {
+			if (this.hasCycle()) {
+				return;
+			} else if (!marked[w]) {
+				edgeTo[w] = v;
+				dfs(G, w);
+			} else if (onStack[w]) {
+				cycle = new Stack<Integer>();
+				for (int x = v; x != w; x = edgeTo[x]) {
+					cycle.push(x);
+				}
+
+				cycle.push(w);
+				cycle.push(v);
+			}
+		}
+		onStack[v] = false;
+	}
+
+	public boolean hasCycle() {
+		return cycle != null;
+	}
+
+	public Iterable<Integer> cycle() {
+		return cycle;
+	}
+}
