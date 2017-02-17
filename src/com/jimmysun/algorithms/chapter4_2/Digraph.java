470fffe8bc5ed4de4e917871be7401490de688a6
Jimmy Sun <jimmysuncpt@gmail.com>
2017-02-17 09:08:20 +0800

    Digraph

---
+++ src/com/jimmysun/algorithms/chapter4_2/Digraph.java
@@ -0,0 +1,70 @@
+package com.jimmysun.algorithms.chapter4_2;
+
+import com.jimmysun.algorithms.chapter1_3.Bag;
+
+import edu.princeton.cs.algs4.In;
+
+public class Digraph {
+	private final int V;
+	private int E;
+	private Bag<Integer>[] adj;
+
+	public Digraph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new Bag<Integer>();
+		}
+	}
+
+	public Digraph(In in) {
+		this(in.readInt());
+		int E = in.readInt();
+		for (int i = 0; i < E; i++) {
+			int v = in.readInt();
+			int w = in.readInt();
+			addEdge(v, w);
+		}
+	}
+
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	public void addEdge(int v, int w) {
+		adj[v].add(w);
+		E++;
+	}
+
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+
+	public Digraph reverse() {
+		Digraph R = new Digraph(V);
+		for (int v = 0; v < V; v++) {
+			for (int w : adj(v)) {
+				R.addEdge(w, v);
+			}
+		}
+		return R;
+	}
+
+	@Override
+	public String toString() {
+		String s = V + " vertices, " + E + " edges\n";
+		for (int v = 0; v < V; v++) {
+			s += v + ": ";
+			for (int w : this.adj(v)) {
+				s += w + " ";
+			}
+			s += "\n";
+		}
+		return s;
+	}
+}
