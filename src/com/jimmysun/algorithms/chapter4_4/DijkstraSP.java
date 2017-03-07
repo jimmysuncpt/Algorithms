f929cb4730ce6c7c8cda59db3ce20962e6b0bfa0
Jimmy Sun <jimmysuncpt@gmail.com>
2017-03-07 10:54:13 +0800

    最短路径的Dijkstra算法

---
+++ src/com/jimmysun/algorithms/chapter4_4/DijkstraSP.java
@@ -0,0 +1,61 @@
+package com.jimmysun.algorithms.chapter4_4;
+
+import com.jimmysun.algorithms.chapter1_3.Stack;
+
+import edu.princeton.cs.algs4.IndexMinPQ;
+
+public class DijkstraSP {
+	private DirectedEdge[] edgeTo;
+	private double[] distTo;
+	private IndexMinPQ<Double> pq;
+
+	public DijkstraSP(EdgeWeightedDigraph G, int s) {
+		edgeTo = new DirectedEdge[G.V()];
+		distTo = new double[G.V()];
+		pq = new IndexMinPQ<>(G.V());
+
+		for (int v = 0; v < G.V(); v++) {
+			distTo[v] = Double.POSITIVE_INFINITY;
+		}
+		distTo[s] = 0.0;
+
+		pq.insert(s, 0.0);
+		while (!pq.isEmpty()) {
+			relax(G, pq.delMin());
+		}
+	}
+
+	private void relax(EdgeWeightedDigraph G, int v) {
+		for (DirectedEdge e : G.adj(v)) {
+			int w = e.to();
+			if (distTo[w] > distTo[v] + e.weight()) {
+				distTo[w] = distTo[v] + e.weight();
+				edgeTo[w] = e;
+				if (pq.contains(w)) {
+					pq.changeKey(w, distTo[w]);
+				} else {
+					pq.insert(w, distTo[w]);
+				}
+			}
+		}
+	}
+
+	public double distTo(int v) {
+		return distTo[v];
+	}
+
+	public boolean hasPathTo(int v) {
+		return distTo[v] < Double.POSITIVE_INFINITY;
+	}
+
+	public Iterable<DirectedEdge> pathTo(int v) {
+		if (!hasPathTo(v)) {
+			return null;
+		}
+		Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+		for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+			path.push(e);
+		}
+		return path;
+	}
+}
