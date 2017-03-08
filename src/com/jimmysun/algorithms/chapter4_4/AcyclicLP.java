f82dd75e417c8f303cf4f25c4305689d4188015f
Jimmy Sun <jimmysuncpt@gmail.com>
2017-03-08 20:52:45 +0800

    无环加权有向图的最长路径算法

---
+++ src/com/jimmysun/algorithms/chapter4_4/AcyclicLP.java
@@ -0,0 +1,52 @@
+package com.jimmysun.algorithms.chapter4_4;
+
+import com.jimmysun.algorithms.chapter1_3.Stack;
+
+public class AcyclicLP {
+	private DirectedEdge[] edgeTo;
+	private double[] distTo;
+
+	public AcyclicLP(EdgeWeightedDigraph G, int s) {
+		edgeTo = new DirectedEdge[G.V()];
+		distTo = new double[G.V()];
+
+		for (int v = 0; v < G.V(); v++) {
+			distTo[v] = Double.NEGATIVE_INFINITY;
+		}
+		distTo[s] = 0.0;
+
+		Topological top = new Topological(G);
+		for (int v : top.order()) {
+			relax(G, v);
+		}
+	}
+
+	private void relax(EdgeWeightedDigraph G, int v) {
+		for (DirectedEdge e : G.adj(v)) {
+			int w = e.to();
+			if (distTo[w] < distTo[v] + e.weight()) {
+				distTo[w] = distTo[v] + e.weight();
+				edgeTo[w] = e;
+			}
+		}
+	}
+
+	public double distTo(int v) {
+		return distTo[v];
+	}
+
+	public boolean hasPathTo(int v) {
+		return distTo[v] < Double.POSITIVE_INFINITY;
+	}
+
+	public Iterable<DirectedEdge> pathTo(int v) {
+		if (!hasPathTo(v)) {
+			return null;
+		}
+		Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+		for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+			path.push(e);
+		}
+		return path;
+	}
+}
