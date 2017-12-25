165e53c116b9d2c166adf15fe5430c123ce61b59
Jimmy Sun <jimmysuncpt@gmail.com>
2017-12-25 14:00:57 +0800

    练习 1.1.14

--- src/com/jimmysun/algorithms/chapter1_1/Ex14.java
+++ src/com/jimmysun/algorithms/chapter1_1/Ex14.java
@@ -1,16 +1,7 @@
 package com.jimmysun.algorithms.chapter1_1;
 
 public class Ex14 {
-	public static int lg(int N) {
-        int i, n;
-        for (i = 0, n = 1; n <= N; i++) {
-            n *= 2;
-        }
-        return i - 1;
-    }
-    
-    private static int biggestIntLessLog2N(int n) {
-        //idea 实际上可以看成是求 2 进制表示法时的副产品
+    private static int lg(int n) {
         int shiftRightCount = 0;
         do {
             n >>= 1;
@@ -21,7 +12,6 @@ public class Ex14 {
     
     public static void main(String[] args) {
         int value = 1025;
-        System.out.printf("the result of method biggestIntLessLog2N is :%s\r\n", biggestIntLessLog2N(value));
         System.out.printf("the result of method log is :%s\r\n", lg(value));
     }
 }
